a82ecb60b8c209abf80aa131ba69152adf6d773d
Vaishali-Tapaswi <52520564+Vaishali-Tapaswi@users.noreply.github.com>
2020-12-01 13:10:58 +0530

    Update Lab2.java

--- demo2/Lab2.java
+++ demo2/Lab2.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 class Lab2Helper extends Thread{
@@ -8,15 +9,22 @@ class Lab2Helper extends Thread{
 	}
 	@Override
 	public void run() {
+		
 		for (int i = 0;i < 50;i++){
-					list.add(this.currentThread().getName() +"" +i);
+		//	synchronized (list) {
+				list.add(this.currentThread().getName() +"" +i);
+		//	}
+					
 		}
 		System.out.println("Current Size " + list.size() + " for thread " + this.currentThread().getName());
 	}
 }
 public class Lab2 {
 public static void main(String[] args) {
-	List<String> list = new ArrayList<>();
+// Option 1 - Normal List + Synchronized Block
+//	List<String> list = new ArrayList<>();
+// Option 2 - Synchronized Collections	
+	List<String> list =Collections.synchronizedList(new ArrayList<>());
 	Lab2Helper t1 = new Lab2Helper(list);
 	Lab2Helper t2 = new Lab2Helper(list);
 	Lab2Helper t3 = new Lab2Helper(list);
